f499aba5e47d4e80d533a74decfa974f5fb87866
mohammadsajadi94 <sajadimohammad50@gmail.com>
2022-07-06 16:04:02 +0330

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import './App.css';
+
+ const App=()=> {
+  return (
+    <div className="App">
+      <h3>اپلیکیشن مدیریت مخاطبین</h3>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
